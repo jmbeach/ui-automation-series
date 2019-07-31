ae690a748e7955bdecdad06f00179314e32fcee0
Jared Beach <jaredbeachdesign@gmail.com>
2019-07-30 22:17:59 -0500

    adds textbox and changing label to wpf app

--- src/DemoWpfApp/MainWindow.xaml.cs
+++ src/DemoWpfApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DemoWpfApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlockName.Text = TextBoxName.Text;
+        }
     }
 }
